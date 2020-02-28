7926b74d4ee933589881b486ef29df4c3a62abe6
Céline Begne <celine.begne@doctrine.fr>
2020-02-28 19:01:03 +0100

    push

---
+++ src/confirm/accepted/ConfirmAccepted.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const TopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const AdviceTitle = styled.div`
+  font-family: 'Sharp Comp';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 110%;
+  text-transform: uppercase;
+  color: #ff7793;
+`;
