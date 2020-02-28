49419249e46c53f758228f6a5c282e9b2fbd5d38
Céline Begne <celine.begne@doctrine.fr>
2020-02-28 09:35:50 +0100

    typo calibre

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './_reset.css';
+import './fonts.css';
 import { Router } from './router/Router';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
