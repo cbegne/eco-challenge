62e9287fae6752515fcaa79a8a82aa83e60a2f07
Céline Begne <celine.begne@doctrine.fr>
2020-02-28 21:21:02 +0100

    share

--- src/confirm/accepted/SharePage.js
+++ src/confirm/accepted/SharePage.js
@@ -1,36 +1,46 @@
 import React from 'react';
-import { TopContainer, Subtitle } from './SharePage.style.js';
+import {
+  TopContainer,
+  Subtitle,
+  SubtitleBottom,
+  Coach,
+  Container,
+} from './SharePage.style.js';
 import { ReturnButton } from '../../challenge/components/ReturnButton';
 import { Title } from '../../challenge/components/Title';
+import { coachList } from '../../constants';
 
 export const SharePage = ({ infos, goBack }) => {
   const { length } = infos.supporters;
+  const coachInfos = coachList.find(({ id, name }) => infos.coach === id);
+
   return (
-    <>
+    <Container>
       <TopContainer>
         <ReturnButton onClick={goBack} />
         <Subtitle>Screenshot et partage</Subtitle>
       </TopContainer>
       <Title>
         {infos.challenger.name} me défie de manger végétarien pendant{' '}
-        {infos.duration} jours
+        <span style={{ color: '#FF7793' }}>{infos.duration} jours</span>
       </Title>
-      <img src="img/lavache.png" alt="challenge" />
+      <img src="img/accepted.png" alt="accept" style={{ maxWidth: '200px' }} />
       <img
-        src="img/accepted.png"
-        alt="accept"
-        style={{ maxWidth: '200px', marginTop: '-100px' }}
+        src={coachInfos.src}
+        alt="challenge"
+        style={{ maxWidth: '200px', marginTop: '-25px' }}
       />
-      <Subtitle>
+      <Coach>avec coach {coachInfos.name}</Coach>
+      <SubtitleBottom>
         Merci{' '}
         {length === 1
           ? `au supporter ${infos.supporters[0].name} `
           : `aux supporters ${infos.supporters[0].name}, ${
               infos.supporters[1].name
             }${infos.supporters[2] ? `, ${infos.supporters[2].name}` : ''} `}
-        pour le soutien 💪
-      </Subtitle>
+        pour le soutien{'\u00A0'}💪
+      </SubtitleBottom>
       <img src="img/logo.png" alt="logo" style={{ maxWidth: '100px' }} />
-    </>
+    </Container>
   );
 };
--- src/confirm/accepted/SharePage.style.js
+++ src/confirm/accepted/SharePage.style.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const Subtitle = styled.div`
   font-family: Calibre;
   font-style: normal;
@@ -17,5 +23,29 @@ export const TopContainer = styled.div`
   flex-direction: row;
   width: 100%;
   align-items: center;
-  margin-bottom: 75px;
+  margin-bottom: 70px;
+`;
+
+export const SubtitleBottom = styled.div`
+  font-family: Calibre;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 120%;
+  color: #574f62;
+  text-align: center;
+  margin-bottom: 15px;
+`;
+
+export const Coach = styled.div`
+  font-family: 'Sharp Comp';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 110%;
+  text-align: center;
+  text-transform: uppercase;
+  color: #370972;
+  margin-top: 5px;
+  margin-bottom: 15px;
 `;
