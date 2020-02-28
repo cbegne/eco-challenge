79e0cda3a78619d4de9dd2c3f39361e7aa041709
nmalzieu <noe.malzieu@gmail.com>
2020-02-28 20:00:40 +0100

    Add a timer

--- src/confirm/accepted/ConfirmAccepted.js
+++ src/confirm/accepted/ConfirmAccepted.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import moment from 'moment';
+import moment from 'moment-timezone';
+import humanizeDuration from 'humanize-duration';
+
 import { Title } from '../../challenge/components/Title';
 import { Subtitle } from '../../challenge/components/Subtitle';
 import { TopContainer } from './ConfirmAccepted.style.js';
@@ -7,10 +9,32 @@ import { ConfirmAcceptedToday } from './ConfirmAcceptedToday';
 import { Layout } from '../../components/Layout';
 import { Page } from '../../components/Page';
 
+const shortFrenchHumanizer = humanizeDuration.humanizer({
+  language: 'shortFr',
+  languages: {
+    shortFr: {
+      d: () => 'J',
+      h: () => 'H',
+      m: () => 'MIN',
+    },
+  },
+});
+
 export const ConfirmAccepted = ({ infos, id }) => {
-  const today = moment(infos.accepted_at).isSame(moment(), 'day');
-  console.log(today);
-  // add timer
+  const now = moment().tz('Europe/Paris');
+  const today = moment(infos.accepted_at).isSame(now, 'day');
+  const begin = moment(infos.accepted_at)
+    .add(1, 'days')
+    .hours(0)
+    .minutes(0)
+    .seconds(0)
+    .milliseconds(0);
+  const end = moment(begin).add(infos.duration, 'days');
+  const remainingTime = moment.duration(end.diff(now));
+  const readableTimer = shortFrenchHumanizer(remainingTime._milliseconds, {
+    units: ['d', 'h', 'm'],
+    maxDecimalPoints: 0,
+  });
   return (
     <div>
       {!infos.accepted_at || today ? (
@@ -25,7 +49,7 @@ export const ConfirmAccepted = ({ infos, id }) => {
                 alt="jc"
                 style={{ maxWidth: ' 175px', marginBottom: '20px' }}
               />
-              <Title>Encore xxx avant la victoire</Title>
+              <Title>Encore {readableTimer} avant la victoire</Title>
               <Subtitle>Tu peux le faire {infos.challenged.name} !</Subtitle>
             </TopContainer>
           </Page>
